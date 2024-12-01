6bbd81566e0588e8373ccd54fda0773b723d49a4
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-12-02 00:20:18 +0100

    added renderer and moved init to own function

--- pong.cpp
+++ pong.cpp
@@ -1,33 +1,19 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_error.h>
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 #include <iostream>
 
+bool init();
+void kill();
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
 int main() {
 
-  SDL_Surface *winSurface = NULL;
-  SDL_Window *window = NULL;
-
-  if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-    std::cerr << "Error initializing SDL : " << SDL_GetError() << std::endl;
-    system("pause");
-    return 1;
-  }
-
-  window =
-      SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                       800, 800, SDL_WINDOW_SHOWN);
-  if (!window) {
-    std::cerr << "Error creating window :" << SDL_GetError() << std::endl;
-    system("pause");
-    return 1;
-  }
-
-  winSurface = SDL_GetWindowSurface(window);
-  if (!winSurface) {
-    std::cerr << "Error getting surface :" << SDL_GetError() << std::endl;
-    system("pause");
+  if (!init()) {
     return 1;
   }
 
@@ -42,7 +28,41 @@ int main() {
     }
   }
 
+  kill();
+  return 0;
+}
+
+bool init() {
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+    std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
+    system("pause");
+    return false;
+  }
+
+  window =
+      SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                       800, 800, SDL_WINDOW_SHOWN);
+  if (!window) {
+    std::cerr << "Error creating window: " << SDL_GetError() << std::endl;
+    system("pause");
+    return false;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (!renderer) {
+    std::cerr << "Error creating renderer: " << SDL_GetError() << std::endl;
+    system("pause");
+    return false;
+  }
+
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderClear(renderer);
+  return true;
+}
+
+void kill() {
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
-  return 0;
 }
