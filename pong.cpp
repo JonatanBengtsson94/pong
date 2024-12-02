98eafa01a6de60d733c471b2f266ac484151c0f5
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-12-02 11:01:44 +0100

    created racket gameobject

--- pong.cpp
+++ pong.cpp
@@ -7,17 +7,27 @@
 #include <iostream>
 
 bool init();
+void setup();
 bool loop();
 void kill();
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
+struct racket {
+  int x;
+  int y;
+  int width;
+  int height;
+} racket;
+
 int main() {
 
   if (!init()) {
     return 1;
   }
+
+  setup();
 
   while (loop()) {
     SDL_Delay(10);
@@ -54,22 +64,35 @@ bool init() {
   return true;
 }
 
+void setup() {
+  racket.x = 100;
+  racket.y = 100;
+  racket.width = 50;
+  racket.height = 100;
+}
+
 bool loop() {
   SDL_Event e;
 
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
-  SDL_Rect racket = {200, 200, 400, 400};
-
+  // Event Loop
   while (SDL_PollEvent(&e) != 0) {
     if (e.type == SDL_QUIT) {
       return false;
     }
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &racket);
-    SDL_RenderPresent(renderer);
   }
+
+  // Render
+  // Background
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  // Racket
+  SDL_Rect racket_rect = {racket.x, racket.y, racket.width, racket.height};
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderFillRect(renderer, &racket_rect);
+
+  // Display
+  SDL_RenderPresent(renderer);
   return true;
 }
 
